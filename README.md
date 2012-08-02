c261bf997610120f78da97e75a3a3ec85bfd9899
Kevin Rockwood <kevin@rockwood.me>
2012-08-02 11:20:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+A drop-in replacement for the default Grunt server. It will serve up coffeescript files as javascript. So if you have a file in app/init.coffee, hitting http://localhost:8000/app/init.js will serve up the compiled version of that file.
+
+Setup
+=======
+
+You'll need to install grunt-coffee first:
+
+    npm install grunt-coffeepot
+
+Then modify your grunt.js file by adding the following line:
+
+    grunt.loadNpmTasks('grunt-coffeecup');
+
+Then add some configuration for the plugin like so:
+
+    grunt.initConfig({
+      ...
+      coffeecup: {
+        port: 8000
+        base: ./app
+      },
+      ...
+    });
+
+defaults:
+  port: 8000
+  base: grunt.js directory
+
+To create a continuously running server use:
+    
+    grunt.registerTask('serve', 'coffeecup watch');
+
---
+++ tasks/coffeecup.js
@@ -0,0 +1,49 @@
+/*
+ * grunt
+ * https://github.com/cowboy/grunt
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+module.exports = function(grunt) {
+
+  // Nodejs libs.
+  var path = require('path');
+
+  // External libs.
+  var connect = require('connect');
+  var coffeepot = require('coffeepot');
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  grunt.registerTask('coffeepot', 'Start a static web server, and serve .coffee files', function() {
+    // Get values from config, or use defaults.
+
+    var port = grunt.config('coffeepot.port') || 8000;
+    
+    var base = path.resolve(grunt.config('coffeepot.base') || '.');
+
+    var middleware = [
+      coffeepot(base),
+      connect.static(base),
+      connect.directory(base)
+    ];
+
+    // If --debug was specified, enable logging.
+    if (grunt.option('debug')) {
+      connect.logger.format('grunt', ('[D] server :method :url :status ' +
+        ':res[content-length] - :response-time ms').magenta);
+      middleware.unshift(connect.logger('grunt'));
+    }
+
+    // Start server.
+    grunt.log.writeln('Starting static web server on port ' + port + '.');
+    connect.apply(null, middleware).listen(port);
+    
+  });
+
+};
